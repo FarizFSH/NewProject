14ce961d236f2bec4cee25bfd6b6df49a27778ad
FarizFSH <farizshirinov1989@gmail.com>
2025-11-23 20:47:00 +0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public class Main {
+         public static void main(String[] args) {
+             // Dəyişənlərin elan edilməsi
+             String studentName = "Orxan";
+             int age = 20;
+             double averageScore = 85.5;
+             boolean isPassed = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+             // Hesab əməliyyatı
+             int bonus = 5;
+             double finalScore = averageScore + bonus;
 
-        System.out.println("asdasd");
+             // Ekrana çap etmək
+             System.out.println("Tələbə: " + studentName);
+             System.out.println("Yaş: " + age);
+             System.out.println("Yekun Bal: " + finalScore);
+
+             // Müqayisə və Məntiq
+             boolean isScholarship = isPassed && (finalScore > 90);
+             System.out.println("Təqaüd alır? " + isScholarship);
+
+
     }
 }
